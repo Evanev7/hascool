324b90f2681010629709dc419c7af35b834145da
caedesyth <evanev7@gmail.com>
2025-01-27 23:52:08 +0000

    has cooler

--- flake.nix
+++ flake.nix
@@ -4,10 +4,18 @@
   outputs = inputs: let
     system = "x86_64-linux";
     pkgs = inputs.nixpkgs.legacyPackages.${system};
+    haskellPackages = pkgs.haskellPackages;
+    noteTaker = haskellPackages.callCabal2nix "note-taker" ./. {};
   in {
+    packages.${system} = {
+      inherit noteTaker;
+      default = noteTaker;
+    };
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs.haskellPackages; [
-        (ghcWithPackages (ps: with ps; [hostname]))
+      buildInputs = with haskellPackages; [
+        ghc
+        cabal-install
+        haskell-language-server
       ];
     };
   };
